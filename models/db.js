575c0b4efa462c4c003c7df790d0da375fb7e1d6
unknown <sanskarchaurey2000@gmail.com>
2023-10-22 17:01:47 +0530

    first commit

--- models/db.js
+++ models/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 mongoose.set('strictQuery', false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/daily_news")
-    .then(() => console.log("db is connected"))
-    .catch(() => console.log(err));
+mongoose
+    .connect("mongodb://127.0.0.1:27017/daily_news")
+    .then(() => console.log("database is connected!!"))
+    .catch(err => console.log(err.message));
